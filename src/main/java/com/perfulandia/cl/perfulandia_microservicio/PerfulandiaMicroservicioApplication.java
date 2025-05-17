55276796375447e0f08310756a334e9d12f59302
POAPCA02LC0109 <POAPCA02LC0100@POAPCA02LC0109.duoc.acad>
2025-05-17 13:38:38 -0400

    Reapply "Descripción de los cambios"
    This reverts commit 6a4de1b3b62e80c9cd9748349a9f8feec09478a7.

---
+++ src/main/java/com/perfulandia/cl/perfulandia_microservicio/PerfulandiaMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.cl.perfulandia_microservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfulandiaMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfulandiaMicroservicioApplication.class, args);
+	}
+
+}
