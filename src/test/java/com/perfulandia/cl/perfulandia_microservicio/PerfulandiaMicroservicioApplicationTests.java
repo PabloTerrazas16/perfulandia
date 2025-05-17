0a39a2de6663d42438f18ba6e054091ee7de5a2c
POAPCA02LC0109 <POAPCA02LC0100@POAPCA02LC0109.duoc.acad>
2025-05-17 13:14:49 -0400

    Descripción de los cambios

---
+++ src/test/java/com/perfulandia/cl/perfulandia_microservicio/PerfulandiaMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.cl.perfulandia_microservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
