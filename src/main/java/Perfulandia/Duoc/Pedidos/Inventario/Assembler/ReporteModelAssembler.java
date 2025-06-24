d2c566a458b2e4d33bada1c3fb6b522327366c12
PabloTerrazas16 <pa.terrazas@duocuc.cl>
2025-06-23 23:03:55 -0400

    Se crearon los HATEOAS en ReporteModelAssembler y ReporteControllerV2

--- src/main/java/Perfulandia/Duoc/Pedidos/Inventario/Assembler/ReporteModelAssembler.java
+++ src/main/java/Perfulandia/Duoc/Pedidos/Inventario/Assembler/ReporteModelAssembler.java
@@ -1,19 +1,23 @@
 package Perfulandia.Duoc.Pedidos.Inventario.Assembler;
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import Perfulandia.Duoc.Pedidos.Inventario.Controller.ReporteControllerV2;
 import Perfulandia.Duoc.Pedidos.Inventario.Model.Reporte;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class ReporteModelAssembler implements RepresentationModelAssembler<Reporte, EntityModel<Reporte>> {
 
-
     @Override
-    public EntityModel<Reporte> toModel(Reporte reporte){
+    public EntityModel<Reporte> toModel(Reporte reporte) {
         return EntityModel.of(reporte,
-                linkTo(methodOn()))
+                linkTo(methodOn(ReporteControllerV2.class).getReporteById(reporte.getIdReporte())).withSelfRel(),
+                linkTo(methodOn(ReporteControllerV2.class).getAllReportes()).withRel("reportes"),
+                linkTo(methodOn(ReporteControllerV2.class).createReporte(reporte)).withRel("crear-reporte"),
+                linkTo(methodOn(ReporteControllerV2.class).updateReporte(reporte.getIdReporte(), reporte)).withRel("actualizar-reporte"),
+                linkTo(methodOn(ReporteControllerV2.class).deleteReporte(reporte.getIdReporte())).withRel("eliminar-reporte"));
     }
-
 }
--- src/main/java/Perfulandia/Duoc/Pedidos/Inventario/Controller/ReporteControllerV2.java
+++ src/main/java/Perfulandia/Duoc/Pedidos/Inventario/Controller/ReporteControllerV2.java
@@ -1,9 +1,64 @@
 package Perfulandia.Duoc.Pedidos.Inventario.Controller;
 
+import Perfulandia.Duoc.Pedidos.Inventario.Assembler.ReporteModelAssembler;
+import Perfulandia.Duoc.Pedidos.Inventario.Model.Reporte;
+import Perfulandia.Duoc.Pedidos.Inventario.Service.ReporteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@RestController
+@RequestMapping("/api/v2/reportes")
 public class ReporteControllerV2 {
 
+    @Autowired
+    private ReporteService reporteService;
 
+    @Autowired
+    private ReporteModelAssembler assembler;
 
+    @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    public CollectionModel<EntityModel<Reporte>> getAllReportes() {
+        List<EntityModel<Reporte>> reportes = reporteService.listarReportes().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
 
+        return CollectionModel.of(reportes,
+                linkTo(methodOn(ReporteControllerV2.class).getAllReportes()).withSelfRel());
+    }
 
+    @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public EntityModel<Reporte> getReporteById(@PathVariable Long id) {
+        Reporte reporte = reporteService.obtenerReportePorId(id);
+        return assembler.toModel(reporte);
+    }
+
+    @PostMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<EntityModel<Reporte>> createReporte(@RequestBody Reporte reporte) {
+        Reporte newReporte = reporteService.guardarReporte(reporte);
+        return ResponseEntity
+                .created(linkTo(methodOn(ReporteControllerV2.class).getReporteById(newReporte.getIdReporte())).toUri())
+                .body(assembler.toModel(newReporte));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<EntityModel<Reporte>> updateReporte(@PathVariable Long id, @RequestBody Reporte reporte) {
+        reporte.setIdReporte(id);
+        Reporte updatedReporte = reporteService.guardarReporte(reporte);
+        return ResponseEntity.ok(assembler.toModel(updatedReporte));
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<?> deleteReporte(@PathVariable Long id) {
+        reporteService.eliminarReporte(id);
+        return ResponseEntity.noContent().build();
+    }
 }
