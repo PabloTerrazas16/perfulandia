753bc2856dd3e834178c9ddbc9cf3c21bdf05095
Sebastian <sec.diaz@duocuc.cl>
2025-06-27 22:51:24 -0400

    se corrije el titulo para la info mostrada en la pagina local de swagger, se deja como "Perfulandia"

--- src/main/java/Perfulandia/Duoc/Pedidos/Inventario/Config/SwaggerConfig.java
+++ src/main/java/Perfulandia/Duoc/Pedidos/Inventario/Config/SwaggerConfig.java
@@ -13,9 +13,9 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI(){
     return new OpenAPI()
             .info(new Info()
-                    .title("API 2025 Pedidos")
+                    .title("API 2025 Perfulandia")
                     .version("1.0")
-                    .description("Documentación de la API para el sistema de pedidos"));
+                    .description("Documentación de la API para el sistema de Perfulandia"));
 
     }
 
